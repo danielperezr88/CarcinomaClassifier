10c6525067d4ad8eed38cebe40aa8be803985628
danielperezr88 <danielperezr88@gmail.com>
2017-01-13 17:18:59 +0100

    One more minor fix

--- watcher.py
+++ watcher.py
@@ -63,7 +63,8 @@ if __name__ == '__main__':
     logging.info("Started")
 
     save_pid()
-    processes = [path.join('TF-img-API', 'TFWorker-API.py'), path.join('APIConsumer', 'APIConsumer.py')]
+    folderpath = path.basename(path.dirname(path.realpath(__file__)))
+    processes = [path.join(folderpath, 'TF-img-API', 'TFWorker-API.py'), path.join(folderpath, 'APIConsumer', 'APIConsumer.py')]
     end = False
 
     while not end:
